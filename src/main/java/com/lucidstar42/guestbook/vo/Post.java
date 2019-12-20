26d657541ff89738ce352aca6fae3b7621cfa9ac
JINSUNG <lucidstar42@gmail.com>
2019-12-20 18:03:14 +0900

    vo 객체 생성

---
+++ src/main/java/com/lucidstar42/guestbook/vo/Post.java
@@ -0,0 +1,66 @@
+package com.lucidstar42.guestbook.vo;
+
+import java.sql.Date;
+
+public class Post {
+	protected int id;
+	protected String email;
+	protected String password;
+	protected String content;
+	protected Date createdAt;
+	protected Date modifiedAt;
+	
+	public int getId() {
+		return id;
+	}
+	
+	public Post setId(int id) {
+		this.id = id;
+		return this;
+	}
+	
+	public String getEmail() {
+		return email;
+	}
+	
+	public Post setEmail(String email) {
+		this.email = email;
+		return this;
+	}
+	
+	public String getPassword() {
+		return password;
+	}
+	
+	public Post setPassword(String password) {
+		this.password = password;
+		return this;
+	}
+	
+	public String getContent() {
+		return content;
+	}
+	
+	public Post setContent(String content) {
+		this.content = content;
+		return this;
+	}
+	
+	public Date getCreatedAt() {
+		return createdAt;
+	}
+	
+	public Post setCreatedAt(Date createdAt) {
+		this.createdAt = createdAt;
+		return this;
+	}
+	
+	public Date getModifiedAt() {
+		return modifiedAt;
+	}
+	
+	public Post setModifiedAt(Date modifiedAt) {
+		this.modifiedAt = modifiedAt;
+		return this;
+	}
+}
